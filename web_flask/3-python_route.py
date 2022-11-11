4274eabdc4f9c382d979f51718b074815058bc3c
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-11 10:09:51 +0100

    Update

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,7 +21,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """Returns 'C {text}'"""
-    return "C {}".format(text.replace('_', ' ')
+    return "C {}".format(text.replace('_', ' '))
 
 
 @app.route('/python', strict_slashes=False)
