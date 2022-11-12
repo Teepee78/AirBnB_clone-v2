e348caf7573db56e91c52db42038810548f03a49
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-12 23:09:25 +0100

    Update

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def shutdown_session():
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = storage.all(State)
+    for state in states:
+        print(state)
+    return render_template('7-states_list.html')
