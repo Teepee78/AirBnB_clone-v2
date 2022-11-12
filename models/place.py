90d19a6b7be31551aa728e43de061bc8064db245
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-12 23:46:04 +0100

    Update

--- models/place.py
+++ models/place.py
@@ -16,8 +16,8 @@ place_amenities_table = Table(
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-    city_id = Column(Integer, ForeignKey('cities.id'), nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(String(1024), nullable=False, default=0)
