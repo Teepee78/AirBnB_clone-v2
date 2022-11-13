ca27c84cf6224c1146aefb0f419ab7f692550ed5
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-13 15:21:36 +0100

    Update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""This module defines and runs a flask application"""
 from flask import Flask, render_template
 from models import storage
 
@@ -6,17 +7,18 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def shutdown_session(request):
+def shutdown_session(exception):
+    """
+    Removes SQLAlchemy session
+    """
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
+    """Renders states list"""
     states = storage.all('State')
-    print(len(states))
-    for state in states:
-        print(state)
-    return render_template('7-states_list.html')
+    return render_template('7-states_list.html', states=states)
 
 
 
