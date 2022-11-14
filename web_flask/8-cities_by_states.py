ed2ba88c476deb0ab2e07dce27033d659d4226a0
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-14 03:01:00 +0100

    Update

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module defines and runs a flask application"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def shutdown_session(exception):
+    """
+    Removes SQLAlchemy session
+    """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Renders states list"""
+    states = storage.all('State')
+    print("Yes")
+    print(states)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+	
+
+
+if __name__ == '__main__':
+    app.run()
