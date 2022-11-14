11b97a547c0979d77411229dfc45660eb4b5c95b
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-14 04:06:47 +0100

    Update

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -23,6 +23,7 @@ def states_list():
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
+    """Renders states and related cities"""
     states = storage.all('State')
     return render_template('8-cities_by_states.html', states=states)
 
