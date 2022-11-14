ff00e64b0c62f9398a6dd7bedbdd811c8af029a6
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-14 04:23:17 +0100

    Update

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -6,19 +6,19 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """Renders states and related cities"""
-    states = storage.all('State')
-    return render_template('8-cities_by_states.html', states=states)
-
-
 @app.teardown_appcontext
 def shutdown_session(exception):
     """
     Removes SQLAlchemy session
     """
     storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Renders states list"""
+    states = storage.all('State')
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
